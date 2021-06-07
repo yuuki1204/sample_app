27eabdc48c6f3149e9be4abcce54af5d145da36f
yuuki1204 <vocalistyuuki@gmail.com>
2021-06-07 16:45:33 +0000

    2nd commit

---
+++ app/controllers/todolists_controller.rb
@@ -0,0 +1,18 @@
+class TodolistsController < ApplicationController
+  def new
+  end
+def create
+    # １. データを新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    # ２. データをデータベースに保存するためのsaveメソッド実行
+    list.save
+    # ３. トップ画面へリダイレクト
+    redirect_to '/top'
+  end
+
+  private
+  # ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body)
+  end
+end
