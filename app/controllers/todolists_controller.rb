1b77a86d71994cd37cbb34fd4796d6dee625e317
yuuki1204 <vocalistyuuki@gmail.com>
2021-06-08 15:00:57 +0000

    5th commit

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -32,6 +32,6 @@ end
   
 private
  def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
  end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '3a2140e65d6dbe40fe34384f09303e8433700a519271abf50fb1656211ed4b2be732f1f6204beb0e443efb7b7b40db9b6918291420435f067bee4fbe41c88448'
