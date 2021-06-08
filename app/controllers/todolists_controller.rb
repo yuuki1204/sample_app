779fd5a28757bfbb7ffe8c2d24711aed0bb350d1
yuuki1204 <vocalistyuuki@gmail.com>
2021-06-08 09:48:07 +0000

    3rd commit

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,15 +1,29 @@
 class TodolistsController < ApplicationController
   def new
+   @list = List.new
   end
-def create
+  
+  def create
     # １. データを新規登録するためのインスタンス作成
     list = List.new(list_params)
     # ２. データをデータベースに保存するためのsaveメソッド実行
     list.save
     # ３. トップ画面へリダイレクト
-    redirect_to '/top'
+    redirect_to todolist_path(list.id)
+  end
+  
+  def index
+   @lists = List.all
   end
 
+def show
+    @list = List.find(params[:id])  
+end
+    
+    
+    def edit
+    @list = List.find(params[:id])
+  　end
   private
   # ストロングパラメータ
   def list_params
