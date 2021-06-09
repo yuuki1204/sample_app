ab029dfe5c2b60bfe112f5c2358f30eb781ad723
yuuki1204 <vocalistyuuki@gmail.com>
2021-06-09 06:20:24 +0000

    6th commit

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -29,6 +29,12 @@ def update
     list.update(list_params)
     redirect_to todolist_path(list.id)
 end
+
+def destroy
+   list = List.find(params[:id])  # データ（レコード）を1件取得
+    list.destroy  # データ（レコード）を削除
+    redirect_to todolists_path  # 投稿一覧画面へリダイレクト
+end
   
 private
  def list_params
